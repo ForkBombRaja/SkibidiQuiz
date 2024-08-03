f2cd04d2d53bde6e76c15c42c745e2ad672648be
Fork Bomb Raja <adityamukerjee621311@gmail.com>
2024-08-03 02:05:03 -0600

    Added UML of the internal game architecture

--- SkibidiQuiz/dialog.cpp
+++ SkibidiQuiz/dialog.cpp
@@ -1,13 +1,16 @@
 #include "dialog.h"
 #include "ui_dialog.h"
-
+/**
+ * @brief This is the implementation class for dialog.h
+ * @author Aditya Mukerjee
+ * @version 1.0
+ */
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
 }
-
 Dialog::~Dialog()
 {
     delete ui;
